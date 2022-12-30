5801f293b33407c2fa83307f4acc3f4db1706d51
GP10Dev <104908091+GP10Dev@users.noreply.github.com>
2022-12-30 15:03:56 +0300

    Update setcon.php

--- server/handelers/setcon.php
+++ server/handelers/setcon.php
@@ -1,5 +1,5 @@
 <?php
-    $con = mysqli_connect("127.0.0.1","root","","police");
+    $con = mysqli_connect("localhost","root","","police");
 
     if(!$con){
         die("failed to link to server. Error code 404x1"); //x1 means failed on step one of connection
